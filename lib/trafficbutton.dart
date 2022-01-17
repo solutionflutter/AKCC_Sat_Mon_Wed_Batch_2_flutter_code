cfadb09f32f56ad6eedff678b41a93a78f1a210d
solutionflutter <solutionflutter@gmail.com>
2022-01-17 15:58:23 +0600

    second push

--- lib/trafficbutton.dart
+++ lib/trafficbutton.dart
@@ -9,6 +9,7 @@ class TrafficButton extends StatefulWidget {
 }
 
 class _TrafficButtonState extends State<TrafficButton> {
+  int change = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,24 +26,52 @@ class _TrafficButtonState extends State<TrafficButton> {
                   width: 100,
                   height: 50,
                   decoration: BoxDecoration(
-                    color: Colors.blue.shade900,
+                    color: change == 1
+                        ? Colors.red.shade900
+                        : Colors.blue.shade900,
                   ),
                 ),
                 Container(
                   width: 100,
                   height: 50,
                   decoration: BoxDecoration(
-                    color: Colors.blue.shade900,
+                    color: change == 2
+                        ? Colors.yellow.shade900
+                        : Colors.blue.shade900,
                   ),
                 ),
                 Container(
                   width: 100,
                   height: 50,
                   decoration: BoxDecoration(
-                    color: Colors.blue.shade900,
+                    color: change == 3
+                        ? Colors.green.shade900
+                        : Colors.blue.shade900,
                   ),
                 )
               ],
+            ),
+            Container(
+              height: 50,
+              width: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.blue.shade900,
+              ),
+              child: FlatButton(
+                onPressed: () {},
+                child: Center(
+                  child: Text(
+                    "Button",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w900,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
